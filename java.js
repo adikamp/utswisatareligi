3e306f2634d8920eb794f5d8b4cb6323a8d401d7
adikamp <adikaprasetyo9@gmail.com>
2024-10-31 21:19:25 +0700

    first upload web
    di upload dari apk vscode

---
+++ java.js
@@ -0,0 +1,4 @@
+// Misalnya, tambahkan efek sederhana ketika halaman selesai dimuat.
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website Wisata Religi siap digunakan!");
+});
